92d5d28c246d0a31693b5366943c095e61d6465a
Adlet Musabaev <adletmusabaev@mail.ru>
2025-06-01 09:56:49 +0500

    Fixing6

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
+// app.js
 const express = require('express');
 const app = express();
 
 app.use(express.json());
 
 let items = [];
+
+app.get('/', (req, res) => {
+  res.send('Hello World'); // новый маршрут
+});
 
 app.get('/items', (req, res) => {
   res.json(items);
